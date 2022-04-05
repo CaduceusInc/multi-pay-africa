c1c917e76d0494084bc41c49dac81c6653803e51
Ifesinachi Ugwuoke <ifexalex2@gmail.com>
2022-04-05 22:38:33 +0100

    added subaccount

--- paystack/customer.py
+++ paystack/customer.py
@@ -1,57 +1,57 @@
 from django.apps import apps
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import UserManager
-from django.contrib.auth.hashers import make_password
+# from django.utils.translation import gettext_lazy as _
+# from django.contrib.auth.models import UserManager
+# from django.contrib.auth.hashers import make_password
 
 
-class CustomerManager(UserManager):
-    def _create_user(self, email, first_name, last_name, password, **extra_fields):
-        """
-        Create and save a user with the given username, email, and password.
-        """
-        if not email:
-            raise ValueError("The given email must be set")
-        email = self.normalize_email(email)
-        # Lookup the real model class from the global app registry so this
-        # manager method can be used in migrations. This is fine because
-        # managers are by definition working on the real model.
-        GlobalUserModel = apps.get_model(
-            self.model._meta.app_label, self.model._meta.object_name
-        )
-        username = GlobalUserModel.normalize_username(username)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.password = make_password(password)
-        user.save(using=self._db)
-        return user
+# class CustomerManager(UserManager):
+#     def _create_user(self, email, first_name, last_name, password, **extra_fields):
+#         """
+#         Create and save a user with the given username, email, and password.
+#         """
+#         if not email:
+#             raise ValueError("The given email must be set")
+#         email = self.normalize_email(email)
+#         # Lookup the real model class from the global app registry so this
+#         # manager method can be used in migrations. This is fine because
+#         # managers are by definition working on the real model.
+#         GlobalUserModel = apps.get_model(
+#             self.model._meta.app_label, self.model._meta.object_name
+#         )
+#         username = GlobalUserModel.normalize_username(username)
+#         user = self.model(username=username, email=email, **extra_fields)
+#         user.password = make_password(password)
+#         user.save(using=self._db)
+#         return user
 
-    def create_user(
-        self, email=None, first_name=None, last_name=None, password=None, **extra_fields
-    ):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, first_name, last_name, password, **extra_fields)
+#     def create_user(
+#         self, email=None, first_name=None, last_name=None, password=None, **extra_fields
+#     ):
+#         extra_fields.setdefault("is_staff", False)
+#         extra_fields.setdefault("is_superuser", False)
+#         return self._create_user(email, first_name, last_name, password, **extra_fields)
 
-    def create_superuser(
-        self, email=None, first_name=None, last_name=None, password=None, **extra_fields
-    ):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
+#     def create_superuser(
+#         self, email=None, first_name=None, last_name=None, password=None, **extra_fields
+#     ):
+#         extra_fields.setdefault("is_staff", True)
+#         extra_fields.setdefault("is_superuser", True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+#         if extra_fields.get("is_staff") is not True:
+#             raise ValueError("Superuser must have is_staff=True.")
+#         if extra_fields.get("is_superuser") is not True:
+#             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, first_name, last_name, password, **extra_fields)
+#         return self._create_user(email, first_name, last_name, password, **extra_fields)
 
 
-class Customer(AbstractUser):
-    username = None
-    email = models.EmailField(_("email address"), unique=True)
+# class Customer(AbstractUser):
+#     username = None
+#     email = models.EmailField(_("email address"), unique=True)
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = ["first_name", "last_name"]
 
-    objects = CustomerManager()
+#     objects = CustomerManager()
