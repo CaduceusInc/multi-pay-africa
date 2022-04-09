9d6bc7df8a11ad02032dcd06013107124b3fa444
CaduceusInc <opakunlejo@gmail.com>
2022-04-09 22:02:42 +0100

    add url for bulk charge -- pre-pull for the week

--- paystack/urls.py
+++
@@ -1,7 +0,0 @@
-from .bulk_charges.views import *
-from django.urls import path
-
-urlpatterns = [
-    path('initialize-charge/bulk/', InitiateBulkCharge.as_view(), name='initialize-charge-bulk'),
-]
-
